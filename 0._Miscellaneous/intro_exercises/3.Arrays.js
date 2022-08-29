5f9d62110b65623d86e52fc384d9efbab0b76580
Rasmus-Kibshede <2740Ralle@gmail.com>
2022-08-29 13:59:20 +0200

    Added and finished exercises strings, objects and arrays

---
+++ 0._Miscellaneous/intro_exercises/3.Arrays.js
@@ -0,0 +1,113 @@
+// --------------------------------------
+// Arrays, for loops
+// --------------------------------------
+// Exercise 1 - Array Positioning
+
+const letters = ["a", "b", "c"];
+// show b in the console 
+
+console.log(letters[1]);
+
+// --------------------------------------
+// Exercise 2 - Array Positioning
+
+const friends = [];
+
+// What a lonely array. Add at least 3 friend objects to it without touching the line above.  
+
+const friendOne = {
+    name: "Tom"
+}
+
+const friendTwo = {
+    name: "Jerry"
+}
+
+const friendThree = {
+    name: "None"
+}
+
+friends[0] = friendOne;
+friends[1] = friendTwo;
+friends[2] = friendThree;
+
+
+// --------------------------------------
+// Exercise 3 - Get the index of first occurrence of that value. 
+
+const significantMathNumbers = [0, 2.718, 3.14159, 1729];
+
+// You want to programmatically find where the number 1729 is in the array.
+// programmatically means that no finger counting allowed. There is a method for this (finding index based of value). 
+
+const location = significantMathNumbers.indexOf(1729);
+
+console.log(location);
+
+
+// --------------------------------------
+// Exercise 4 - Inserting elements
+
+const diet = ["tomato", "cucumber", "rocket"];
+
+// You are a programmer. In one line (one statement) insert hamburger, soda and pizza between the elements cucumber and rocket
+
+//insert hamburger, soda and pizza
+
+diet.splice(2, 0, "hamburger", "soda", "pizza")
+console.log(diet);
+
+
+// --------------------------------------
+// Exercise 5 - Remove element
+
+// Remove the LAST element of the array.
+// Don't remove by index. You know in advance that it's the last in the array because you are too full already. 
+
+diet.pop();
+
+console.log(diet);
+
+
+// --------------------------------------
+// Exercise 6 - Copy array
+
+// You really like your daily diet from last exercise. Copy it to a new array called dinnerTray so you can give it to a friend.  
+
+const dinnerTray = diet;
+
+console.log(dinnerTray);
+
+// --------------------------------------
+// Exercise 7 - For loop
+
+const lettersExpanded = ["a", "b", "c", "d", "e", "f", "g", "h"];
+
+// log every second char in the array starting from b
+
+for (let i = 0; i < lettersExpanded.length; i++) {
+    console.log(lettersExpanded[i]);
+    i++;
+};
+
+// --------------------------------------
+// Exercise 8 - For loop and if statement
+
+const numbers  = [5, 3, 2, 7, 11, 12, 0, -20, 6];
+
+const discardedNumbers = [];
+
+// log the element if the number is above 6 or below 0
+// else push them to the array discardedNumbers
+
+
+
+numbers.forEach(element => {
+    if (element > 6 || element < 0) {
+        console.log(element);
+    }else{
+        discardedNumbers.push(element);
+    }
+});
+
+// --------------------------------------
