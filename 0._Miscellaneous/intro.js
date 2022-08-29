b0c3b93f6c84d71e489acf9270774af948239bcc
Rasmus-Kibshede <2740Ralle@gmail.com>
2022-08-29 09:56:38 +0200

    Added the type coercion file and intro exercises basic file

---
+++ 0._Miscellaneous/intro.js
@@ -0,0 +1,13 @@
+//Type coersion lecture
+const total = 34.32;
+const price = 12.32;
+
+const newTotal = total + price;
+
+console.log(newTotal);
+
+const balanceEquals = total === newTotal;
+
+console.log(balanceEquals);
+
+
