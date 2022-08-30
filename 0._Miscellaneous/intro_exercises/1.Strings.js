5cedbc68d283af5a54a5a3bf331f946507e8e68e
Rasmus-Kibshede <2740Ralle@gmail.com>
2022-08-30 10:55:37 +0200

    added some small changes

--- 0._Miscellaneous/intro_exercises/1.Strings.js
+++ 0._Miscellaneous/intro_exercises/1.Strings.js
@@ -35,9 +35,9 @@ const three = 98;
 // Show in the console the avg. with 5 decimals
 
 
-const avg = (one + two + three) / 3;
+const avg = ((one + two + three) / 3).toFixed(5);
 
-console.log(avg.toFixed(5));
+console.log(avg);
 
 
 // --------------------------------------
