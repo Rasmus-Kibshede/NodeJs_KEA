6c6f647019e185e2ad0636466b4109fa0b2c2123
Rasmus-Kibshede <2740Ralle@gmail.com>
2022-09-12 21:45:35 +0200

    Added post, put, delete and reformed code to use import module

--- 1.1_Basic_Express_Weapon/app.js
+++ 1.1_Basic_Express_Weapon/app.js
@@ -1,38 +1,102 @@
 
 //import and instantiate express
-const app = require("express")();
+import express from "express";
+//const express = require("express");
+const app = express();
 
-//Makes a weapon class with a constructor
-class Weapon{
-    constructor(id, name, type, magSize){
-        this.id = id;
-        this.name = name;
-        this.type = type;
-        this.magSize = magSize;
-    };
-};
+app.use(express.json());
 
 //Makes a list of weapons
-const listOfWeapons = [new Weapon(1, "ak-47", "Rifle", 32), new Weapon(2, "Glock9", "Pistol", 8)];
+const listOfWeapons = [
+    {
+        id : 1,
+        name : "ak-47",
+        type : "Rifle",
+        ammo : 32
+    },{
+        id : 2,
+        name : "Glock9",
+        type : "Pistol",
+        ammo : 8
+    },{
+        id : 3,
+        name : "M4",
+        type : "Rifle",
+        ammo : 32
+    }
+];
 
-
+// Frontpage
 app.get("/", (req, res) =>{
     res.send('Welcome to the weapon frontpage');
 });
 
+// GET | Gets all weapons
 app.get("/weapons", (req, res) =>{
     res.send({
         weapons: listOfWeapons
     });
 });
 
+// Finds a weapon from the weaponList
+function findWeaponInList(id) {
+    return listOfWeapons.find(weapon => weapon.id === Number(id));
+};
+
+// GET | Gets a weapon by weapon id
 app.get("/weapons/:id", (req, res) => {
-    let weapon = listOfWeapons.find(weapon => weapon.id === Number(req.params.id))
+    const weapon = findWeaponInList(req.params.id);
     if (weapon === undefined) {
         res.send("can't find weapon with id: " + req.params.id);        
     }else{
-        res.send(weapon);
+        res.send({
+            weapons: weapon
+        });
     }
+});
+
+// POST | Creates a weapon
+app.post("/weapons", (req, res) => {
+    listOfWeapons.push(req.body);
+    res.send({
+        weapons: listOfWeapons
+    });
+});
+
+
+// PUT | Updates a weapons attributes by weapon id
+app.put("/weapons/:id", (req, res) => {
+    const weapon = findWeaponInList(req.params.id);
+
+    if(weapon !== undefined){
+        listOfWeapons[listOfWeapons.indexOf(weapon)] = req.body;
+    };
+
+    res.send({
+        weapons: listOfWeapons
+    });
+});
+
+// Patch | Updates a weapons attribute by weapon id
+app.patch("/weapons/:id", (req, res) => {
+    const weapon = findWeaponInList(req.params.id);
+
+    // MAKE CODE HERE
+
+    res.send({
+        weapons: listOfWeapons
+    });
+});
+
+
+// DELETE | Removes a weapon by id
+app.delete("/weapons/:id", (req, res) => {
+    const weapon = findWeaponInList(req.params.id);
+    listOfWeapons.splice(listOfWeapons.indexOf(weapon), 1);
+
+    res.send({
+        weapons: listOfWeapons
+    });
 });
 
 
