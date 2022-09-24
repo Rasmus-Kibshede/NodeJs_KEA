17992428ea5960667c6aa3a0cdd5da52ced68452
Rasmus-Kibshede <2740Ralle@gmail.com>
2022-09-24 22:20:27 +0200

    setup for deployment. added script start and port env

--- app.js
+++ app.js
@@ -50,4 +50,4 @@ app.get("/dates", (req, res) => {
 });
 
 
-app.listen(8080, () => console.log("The server is running on port", 8080));
+app.listen(process.env.PORT || 8080, () => {console.log("The server is running on port ", 8080)});
