265b867a132eb68764aba1e6d10299c0cb1c3d6b
Rasmus-Kibshede <2740Ralle@gmail.com>
2022-10-10 13:04:13 +0200

    created my first svelte project

--- 8._First_Svelte_Project/src/App.svelte
+++ 8._First_Svelte_Project/src/App.svelte
@@ -1,12 +1,13 @@
 <script>
+    import Date from "./components/Date.svelte";
+
   const name = "Rasmus";
   const age = 29;
-  const currentDate = new Date().toDateString();
 </script>
 
 <h2 id="page-title">First svelte project</h2>
-<div>More content for {name} that is {age} <p>{currentDate}</p></div>
-
+<div>More content for {name} that is {age}</div>
+<Date></Date>
 <style>
   #page-title {
     color: blue;
---
+++ 8._First_Svelte_Project/src/components/Date.svelte
@@ -0,0 +1,17 @@
+<script>
+    let date = new Date();
+
+    let dates = [date];
+
+    function updateDate() {
+        date = new Date();
+        /* dates.push(date);
+        dates = dates; */
+        dates = [...dates, date];
+    }
+</script>
+
+<div>currentdate: {date}</div>
+<button on:click={updateDate}>Click on me</button>
+
+<div>{JSON.stringify(dates)}</div>
