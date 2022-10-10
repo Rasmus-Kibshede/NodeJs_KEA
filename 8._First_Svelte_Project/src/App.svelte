0d5460bc515ecbe4bd48751ec2c85ee3d16051d4
Rasmus-Kibshede <2740Ralle@gmail.com>
2022-10-10 11:29:53 +0200

    Created first svelte project

---
+++ 8._First_Svelte_Project/src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  const name = "Rasmus";
+  const age = 29;
+  const currentDate = new Date().toDateString();
+</script>
+
+<h2 id="page-title">First svelte project</h2>
+<div>More content for {name} that is {age} <p>{currentDate}</p></div>
+
+<style>
+  #page-title {
+    color: blue;
+  }
+</style>
