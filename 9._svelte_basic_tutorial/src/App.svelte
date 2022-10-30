6d7605ba2ebc448da1e3af84c7e7d095992eff5e
Rasmus-Kibshede <2740Ralle@gmail.com>
2022-10-30 22:46:46 +0100

    Svelte basics del 1

--- 9._svelte_basic_tutorial/src/App.svelte
+++ 9._svelte_basic_tutorial/src/App.svelte
@@ -1,34 +1,53 @@
 <script>
     import AddData from "./pages/AddData.svelte";
+    import Assigment from "./pages/Assigment.svelte";
+    import Declarations from "./pages/Declarations.svelte";
     import DynamikAttribute from "./pages/Dynamik_Attribute.svelte";
     import HtmlTags from "./pages/HtmlTags.svelte";
     import MakingAnApp from "./pages/MakingAnApp.svelte";
     import NestedComponents from "./pages/NestedComponents.svelte";
+    import Statements from "./pages/Statements.svelte";
     import Styleling from "./pages/Styleling.svelte";
+    import UpdatingArraysAndObjects from "./pages/Updating-arrays-and-objects.svelte";
 </script>
-
 
 <!-- a. Basic -->
 <!-- <AddData></AddData> -->
 
 <!-- b. Basic -->
 
-
 <!-- c. Basic -->
 <!-- <DynamikAttribute></DynamikAttribute> -->
-
 
 <!-- d. Basic -->
 <!-- <Styleling></Styleling> -->
 
-
 <!-- e. Basic -->
 <!-- <NestedComponents></NestedComponents> -->
-
 
 <!-- f. Basic -->
 <!-- <HtmlTags></HtmlTags> -->
 
 <!-- g. Basic -->
-<MakingAnApp></MakingAnApp>
+<!-- <MakingAnApp></MakingAnApp> -->
+
+<!-- ------------Reactivity------------- -->
+
+<!-- Assigment -->
+<!-- <Assigment /> -->
+
+<!-- Declarations -->
+<!-- <Declarations /> -->
+
+<!-- Statements -->
+<!-- <Statements /> -->
+
+<!-- updating-arrays-and-objects -->
+<UpdatingArraysAndObjects />
+
+
+
+
+
+
 
