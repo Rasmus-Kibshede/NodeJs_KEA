e25cf368b5f89dde63d4736872e16b877feec59f
Rasmus-Kibshede <2740Ralle@gmail.com>
2022-11-19 20:48:21 +0100

    added session loggin backend

--- 12._mandatory_02/server/routers/LoginRouter.js
+++ 12._mandatory_02/server/routers/LoginRouter.js
@@ -10,10 +10,6 @@ async function loginCheck(req, res, next) {
     console.log(user);
     res.status(200).send({ data: user }); */
 
-
-
-
-
     //TODO Database check here
     /* 
     req.session.username = email;
@@ -25,14 +21,25 @@ async function loginCheck(req, res, next) {
 
 
 //TODO lav check på om user findes og set session med middleware
- router.post("/login", async (req, res) => {
-    //console.log(req.body);
+router.post("/login", async (req, res, next) => {
 
-    const user = await db.all("SELECT * FROM users WHERE user_email=? AND user_password=?", [req.body.email, req.body.password]);
+    const user = await db.get("SELECT * FROM users WHERE user_email=? AND user_password=?", [req.body.email, req.body.password]);
+    //console.log(user);
 
-    console.log(user);
-
-    res.status(200).send({ data: user });
+    if (req.session.isLoggedIn) {
+        res.status(200).send({ message: `You are already logged in as ${req.body.email}` });
+    } else {
+        if (user) {
+            next();
+        } else {
+            res.status(400).send({ message: "Wrong email or password" });
+        }
+    }
+}, (req, res) => {
+    req.session.isLoggedIn = true;
+    req.session.email = req.body.email;
+    console.log("session is set");
+    res.status(200).send({ message: `Welcome ${req.body.email}` });
 });
 
 
